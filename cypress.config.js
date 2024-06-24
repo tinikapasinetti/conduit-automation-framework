809606e5fd80f381e62fcc25f6d7e27f3378d115
Tinika Pasinetti <tinikapasinetti@hotmail.com>
2024-06-25 02:10:58 +1000

    Updated spec file pattern for API tests + added login API tests

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const cucumber = require('cypress-cucumber-preprocessor').default
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: 'cypress/e2e/**/*.feature',
+    specPattern: 'cypress/e2e/{api_tests,features}/*.{feature,js}',
     supportFile: false,
     setupNodeEvents(on, config) {
       on('file:preprocessor', cucumber())
@@ -11,6 +11,6 @@ module.exports = defineConfig({
     env: {
       conduitHomeScreen: 'https://demo.realworld.io/#/'
     },
-    defaultCommandTimeout: 10000
+    defaultCommandTimeout: 10000,
   },
 });
---
+++ cypress/e2e/api_tests/signIn.js
@@ -0,0 +1,40 @@
+describe('Sign In API Tests', () => {
+
+    const apiUrl = 'https://conduit.productionready.io/api';
+    const loginPath = '/users/login'
+
+    it('Successful Login', () => {
+        cy.request('POST', apiUrl + loginPath, {
+            user:
+            {
+                email: 'tinikapas@gmail.com',
+                password: 'Test@123'
+            }
+        }).should((response) => {
+            expect(response.status).to.eq(200);
+            expect(response.body.user.email).to.eq('tinikapas@gmail.com');
+            expect(response.body.user.username).to.eq('Tinika1');
+            expect(response.body.user.token).to.be.not.null;
+        });
+    });
+
+    it('Unsuccessful Login', () => {
+        const email = 'email' + String(new Date().getTime());
+
+        cy.request({
+            method: "POST",
+            url: apiUrl + loginPath,
+            failOnStatusCode: false,
+            body:
+            {
+                user: { email: "tinikapasofuhjkl@gmail.com", password: "Test@123" }
+            }
+        }).should((response) => {
+            expect(response.status).to.eq(403);
+        });
+
+
+    });
+
+
+})
