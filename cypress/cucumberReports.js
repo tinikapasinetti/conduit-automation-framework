fffcedc4e292f977540889db7c53ee9bd13d57bc
Tinika Pasinetti <tinikapasinetti@hotmail.com>
2024-06-25 16:48:50 +1000

    Added cucumber report dependency and config

---
+++ cypress/cucumberReports.js
@@ -0,0 +1,6 @@
+const report = require("multiple-cucumber-html-reporter");
+
+report.generate({
+  jsonDir: "./cypress/reports/cucumber-json",
+  reportPath: "./cypress/reports/cucumber-report/",
+});
